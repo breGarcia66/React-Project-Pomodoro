2c3528dc85f7221ba0a88a177737cf96faccc4ae
breGarcia66 <brennol3vy@gmail.com>
2025-09-19 23:09:05 -0300

    Tipagem de props no componente Heading e estilizações no porjeto do pomodoro

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
 
-export function App() {
+import { Heading } from './components/Heading';
+import { TimerIcon } from 'lucide-react';
 
+export function App() {
   return (
     <>
-      <Heading />
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero cum quae eum eos quod, repellat, culpa aperiam debitis quidem voluptatum numquam laborum error ratione voluptates facilis dolorem hic odio aliquam?</p>
+      <Heading>
+        Hello, World
+        <button> 
+          <TimerIcon/>
+        </button>
+      </Heading>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero cum quae
+        eum eos quod, repellat, culpa aperiam debitis quidem voluptatum numquam
+        laborum error ratione voluptates facilis dolorem hic odio aliquam?
+      </p>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 
 import { App } from './App';
 
+import './styles/global.css';0
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
