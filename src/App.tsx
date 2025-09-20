aa77f099f36ae5ffe0caf8c7fc35a7ee1fd5a6e1
breGarcia66 <brennol3vy@gmail.com>
2025-09-19 22:11:01 -0300

    Usando CSS global e CSS modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Heading } from './components/Heading';
+
+import './styles/theme.css';
+import './styles/global.css';
+
 export function App() {
+
   return (
     <>
-      <h1>Hello, World!</h1>
+      <Heading />
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero cum quae eum eos quod, repellat, culpa aperiam debitis quidem voluptatum numquam laborum error ratione voluptates facilis dolorem hic odio aliquam?</p>
     </>
   );
