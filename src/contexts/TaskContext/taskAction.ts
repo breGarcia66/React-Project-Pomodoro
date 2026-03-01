9b799d59c0f2c7c149c0a082a83a8c8478e92c7c
breGarcia66 <brennol3vy@gmail.com>
2026-02-28 21:41:33 -0300

    correção de erros

--- src/contexts/TaskContext/taskAction.ts
+++ src/contexts/TaskContext/taskAction.ts
@@ -2,38 +2,38 @@
 import type { TaskModel } from '../../models/TaskModel';
 import type { TaskStateModel } from '../../models/TaskStateModel';
 
-export enum taskActionType {
-  START_TASK = 'START_TASK',
-  INTERRUPT_TASK = 'INTERRUPT_TASK',
-  RESET_STATE = 'RESET_STATE',
-  COUNT_DOW = 'COUNT_DOWN',
-  COMPLETE_TASK = 'COMPLETE_TASK',
-  CHANGE_SETTINGS = 'CHANGE_SETTINGS',
-}
+export const taskActionType = {
+  START_TASK: 'START_TASK',
+  INTERRUPT_TASK: 'INTERRUPT_TASK',
+  RESET_STATE: 'RESET_STATE',
+  COUNT_DOW: 'COUNT_DOWN',
+  COMPLETE_TASK: 'COMPLETE_TASK',
+  CHANGE_SETTINGS: 'CHANGE_SETTINGS',
+} as const;
 
 type taskActionsWithPayload =
   | {
-      type: taskActionType.START_TASK;
+      type: typeof taskActionType.START_TASK;
       payload: TaskModel;
     }
   | {
-      type: taskActionType.COUNT_DOW;
+      type: typeof taskActionType.COUNT_DOW;
       payload: { secondsRemaining: number };
     }
   | {
-      type: taskActionType.CHANGE_SETTINGS;
+      type: typeof taskActionType.CHANGE_SETTINGS;
       payload: TaskStateModel['config'];
     };
 
 type taskActionsWithoutPayload =
   | {
-      type: taskActionType.RESET_STATE;
+      type: typeof taskActionType.RESET_STATE;
     }
   | {
-      type: taskActionType.INTERRUPT_TASK;
+      type: typeof taskActionType.INTERRUPT_TASK;
     }
   | {
-      type: taskActionType.COMPLETE_TASK;
+      type: typeof taskActionType.COMPLETE_TASK;
     };
 
 export type taskActionsModel =
